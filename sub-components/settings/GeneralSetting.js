1fb733f48917c704a1e22c63a44cfc2e940e6fbf
mattiegisselbeck <gisse015@umn.edu>
2023-11-12 16:42:39 -0600

    Refresh

--- sub-components/settings/GeneralSetting.js
+++ sub-components/settings/GeneralSetting.js
@@ -1,15 +1,19 @@
 // import node module libraries
-import { Col, Row, Form, Card, Button, Image } from "react-bootstrap";
+import { Col, Row, Form, Card, Button, Image } from 'react-bootstrap';
 
 // import widget as custom components
-import { FormSelect, DropFiles } from "widgets";
+import { FormSelect, DropFiles } from 'widgets';
+
+// import hooks
+import useMounted from 'hooks/useMounted';
 
 const GeneralSetting = () => {
+  const hasMounted = useMounted();
   const countryOptions = [
-    { value: "India", label: "India" },
-    { value: "US", label: "US" },
-    { value: "UK", label: "UK" },
-    { value: "UAE", label: "UAE" },
+    { value: 'India', label: 'India' },
+    { value: 'US', label: 'US' },
+    { value: 'UK', label: 'UK' },
+    { value: 'UAE', label: 'UAE' }
   ];
 
   return (
@@ -17,9 +21,7 @@ const GeneralSetting = () => {
       <Col xl={3} lg={4} md={12} xs={12}>
         <div className="mb-4 mb-lg-0">
           <h4 className="mb-1">General Setting</h4>
-          <p className="mb-0 fs-5 text-muted">
-            Profile configuration settings{" "}
-          </p>
+          <p className="mb-0 fs-5 text-muted">Profile configuration settings </p>
         </div>
       </Col>
       <Col xl={9} lg={8} md={12} xs={12}>
@@ -36,23 +38,11 @@ const GeneralSetting = () => {
               <Col md={9}>
                 <div className="d-flex align-items-center">
                   <div className="me-3">
-                    <Image
-                      src="/images/avatar/avatar-5.jpg"
-                      className="rounded-circle avatar avatar-lg"
-                      alt=""
-                    />
+                    <Image src="/images/avatar/avatar-5.jpg" className="rounded-circle avatar avatar-lg" alt="" />
                   </div>
                   <div>
-                    <Button
-                      variant="outline-white"
-                      className="me-2"
-                      type="submit"
-                    >
-                      Change{" "}
-                    </Button>
-                    <Button variant="outline-white" type="submit">
-                      Remove{" "}
-                    </Button>
+                    <Button variant="outline-white" className="me-2" type="submit">Change </Button>
+                    <Button variant="outline-white" type="submit">Remove </Button>
                   </div>
                 </div>
               </Col>
@@ -66,144 +56,74 @@ const GeneralSetting = () => {
               <Col md={9}>
                 {/* dropzone input */}
                 <div>
-                  <Form
-                    action="#"
-                    className="dropzone mb-3 py-10 border-dashed"
-                  >
+                  {hasMounted && <Form action="#" className="dropzone mb-3 py-10 border-dashed">
                     <DropFiles />
-                  </Form>
-                  <Button variant="outline-white" type="submit">
-                    Change{" "}
-                  </Button>
+                  </Form>}
+                  <Button variant="outline-white" type="submit">Change </Button>
                 </div>
               </Col>
             </Row>
             <div>
-              {/* border */}
               <div className="mb-6">
                 <h4 className="mb-1">Basic information</h4>
               </div>
+              {hasMounted && 
               <Form>
-                {/* row */}
                 <Row className="mb-3">
-                  <label
-                    htmlFor="fullName"
-                    className="col-sm-4 col-form-label
-                    form-label"
-                  >
-                    Full name
-                  </label>
-                  <div className="col-sm-4 mb-3 mb-lg-0">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="First name"
-                      id="fullName"
-                      required
-                    />
-                  </div>
-                  <div className="col-sm-4">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Last name"
-                      id="lastName"
-                      required
-                    />
-                  </div>
+                  <Form.Label className="col-sm-4 col-form-label form-label" htmlFor="fullName">Full name</Form.Label>
+                  <Col sm={4} className="mb-3 mb-lg-0">
+                    <Form.Control type="text" placeholder="First name" id="fullName" required />
+                  </Col>
+                  <Col sm={4}>
+                    <Form.Control type="text" placeholder="Last name" id="lastName" required />
+                  </Col>
                 </Row>
                 {/* row */}
                 <Row className="mb-3">
-                  <label
-                    htmlFor="email"
-                    className="col-sm-4 col-form-label
-                    form-label"
-                  >
-                    Email
-                  </label>
-                  <div className="col-md-8 col-12">
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder="Email"
-                      id="email"
-                      required
-                    />
-                  </div>
-                </Row>
-                {/* row */}
-                <Row className="mb-3">
-                  <Form.Label className="col-sm-4" htmlFor="phone">
-                    Phone <span className="text-muted">(Optional)</span>
-                  </Form.Label>
+                <Form.Label className="col-sm-4 col-form-label form-label" htmlFor="email">Email</Form.Label>
                   <Col md={8} xs={12}>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter Phone"
-                      id="phone"
-                    />
+                    <Form.Control type="email" placeholder="Email" id="email" required />
+                  </Col>
+                </Row>
+                {/* row */}
+                <Row className="mb-3">
+                  <Form.Label className="col-sm-4" htmlFor="phone">Phone <span className="text-muted">(Optional)</span></Form.Label>
+                  <Col md={8} xs={12}>
+                    <Form.Control type="text" placeholder="Enter Phone" id="phone" />
                   </Col>
                 </Row>
 
                 {/* Location */}
                 <Row className="mb-3">
-                  <Form.Label className="col-sm-4" htmlFor="country">
-                    Location
-                  </Form.Label>
+                  <Form.Label className="col-sm-4" htmlFor="country">Location</Form.Label>
                   <Col md={8} xs={12}>
-                    <Form.Control
-                      as={FormSelect}
-                      placeholder="Select Country"
-                      id="country"
-                      options={countryOptions}
-                    />
+                    <Form.Control as={FormSelect} placeholder="Select Country" id="country" options={countryOptions} />
                   </Col>
                 </Row>
 
                 {/* Address Line One */}
                 <Row className="mb-3">
-                  <Form.Label className="col-sm-4" htmlFor="addressLine">
-                    Address line 1
-                  </Form.Label>
+                  <Form.Label className="col-sm-4" htmlFor="addressLine">Address line 1</Form.Label>
                   <Col md={8} xs={12}>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter Address line 1"
-                      id="addressLine"
-                      required
-                    />
+                    <Form.Control type="text" placeholder="Enter Address line 1" id="addressLine" required />
                   </Col>
                 </Row>
 
                 {/* Address Line Two */}
                 <Row className="mb-3">
-                  <Form.Label className="col-sm-4" htmlFor="addressLineTwo">
-                    Address line 2
-                  </Form.Label>
+                  <Form.Label className="col-sm-4" htmlFor="addressLineTwo">Address line 2</Form.Label>
                   <Col md={8} xs={12}>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter Address line 2"
-                      id="addressLineTwo"
-                      required
-                    />
+                    <Form.Control type="text" placeholder="Enter Address line 2" id="addressLineTwo" required />
                   </Col>
                 </Row>
 
+
                 {/* Zip code */}
                 <Row className="align-items-center">
-                  <Form.Label className="col-sm-4" htmlFor="zipcode">
-                    Zip code
-                    <i className="fe fe-info fs-4 me-2 text-muted icon-xs"></i>
-                  </Form.Label>
+                  <Form.Label className="col-sm-4" htmlFor="zipcode">Zip code</Form.Label>
 
                   <Col md={8} xs={12}>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter Zip code"
-                      id="zipcode"
-                      required
-                    />
+                    <Form.Control type="text" placeholder="Enter Zip code" id="zipcode" required />
                   </Col>
 
                   <Col md={{ offset: 4, span: 8 }} xs={12} className="mt-4">
@@ -211,14 +131,17 @@ const GeneralSetting = () => {
                       Save Changes
                     </Button>
                   </Col>
+
                 </Row>
               </Form>
+              }
             </div>
           </Card.Body>
         </Card>
+
       </Col>
     </Row>
-  );
-};
+  )
+}
 
-export default GeneralSetting;
+export default GeneralSetting
