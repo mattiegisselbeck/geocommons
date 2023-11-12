4eb9172a777096dc387d671e1e1e2bc91745dff8
mattiegisselbeck <gisse015@umn.edu>
2023-11-12 17:30:44 -0600

    Update

--- sub-components/index.js
+++ sub-components/index.js
@@ -3,12 +3,12 @@
  * so here you will find folder names which are listed in root pages.
  */
 
-// sub components for /pages/dashboard
+// sub-components for /pages/dashboard
 import TasksPerformance from 'sub-components/dashboard/TasksPerformance';
 import Teams from 'sub-components/dashboard/Teams';
 import Display from 'sub-components/dashboard/Display';
 
-// sub components for /pages/profile
+// sub-components for /pages/profile
 import AboutMe from 'sub-components/profile/AboutMe';
 import ActivityFeed from 'sub-components/profile/ActivityFeed';
 import MyTeam from 'sub-components/profile/MyTeam';
@@ -16,19 +16,8 @@ import ProfileHeader from 'sub-components/profile/ProfileHeader';
 import ProjectsContributions from 'sub-components/profile/ProjectsContributions';
 import RecentFromBlog from 'sub-components/profile/RecentFromBlog';
 
-// sub components for /pages/billing
-import CurrentPlan from 'sub-components/billing/CurrentPlan';
-import BillingAddress from 'sub-components/billing/BillingAddress';
 
-// sub components for /pages/settings
-import DeleteAccount from 'sub-components/settings/DeleteAccount';
-import EmailSetting from 'sub-components/settings/EmailSetting';
-import GeneralSetting from 'sub-components/settings/GeneralSetting';
-import Notifications from 'sub-components/settings/Notifications';
-import Preferences from 'sub-components/settings/Preferences';
-
-// sub components for /pages/
-
+// sub-components for /pages/
 
 export {
    Display,
@@ -42,12 +31,5 @@ export {
    ProjectsContributions,
    RecentFromBlog,
 
-   CurrentPlan,
-   BillingAddress,
-
-   DeleteAccount, 
-   EmailSetting,  
-   GeneralSetting, 
-   Notifications, 
-   Preferences
+   
 };
